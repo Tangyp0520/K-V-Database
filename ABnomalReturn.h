3de68a0311f51ed606d37c93426a5912728f5429
2019151091 <73161924+2019151091@users.noreply.github.com>
2020-12-13 15:58:58 +0800

    Delete ABnomalReturn.h

--- ABnomalReturn.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#pragma once
-#define SUCCESS 1
-#define FAILED 0
-#define FILE_OPENIBG_FAILED -1
-#define MEOMORY_ALLOCATION_FAILUER -2
-#define KEY_NOT_EXIST -3
-#define KEY_DELETED -4
-#define ERROR_EXECUTING_SUBFUNCTION -5
